289f2a275b54de52ba5e93314a60312c06343c6e
Nir Naor <nirnaori@gmail.com>
2012-01-06 13:06:32 +0200

    trying to add all of the files in main folder and subfolders to the github

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//$(document).ready(function () {
+//    console.log('will now activate the cross slide plugin');
+
+
+//    $('#slideshow').crossSlide({
+//        sleep: 2,
+//        fade: 1
+//    }, [
+//      { src: 'images/diamonds/diamond1.jpg' },
+//      { src: 'images/diamonds/diamond2.jpg' },
+//      { src: 'images/diamonds/diamond3.jpg' },
+//      { src: 'images/diamonds/diamond4.jpg' }
+//    ]);
+
+//});

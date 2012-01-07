7c3659d06c3e9632092d5979a82d6d6fc946377f
Nir Naor <nirnaori@gmail.com>
2012-01-07 17:53:15 +0200

    Finally managed to add the ken burst effect to the home div

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
-//$(document).ready(function () {
-//    console.log('will now activate the cross slide plugin');
+$(document).ready(function () {
+
+    console.log('hello');
+    $('#gallery').hide();
+
+    $('#slidinggallery').crossSlide({
+        fade: 1
+    }, [
+  {
+      src: 'diamond1.jpg',
+      alt: 'Sand Castle',
+      from: '100% 80% 1x',
+      to: '100% 0% 1.7x',
+      time: 3
+  }, {
+      src: 'diamond2.jpg',
+      alt: 'Sunflower',
+      from: 'top left',
+      to: 'bottom right 1.5x',
+      time: 2
+  }, {
+      src: 'diamond3.jpg',
+      alt: 'Flip Flops',
+      from: '100% 80% 1.5x',
+      to: '80% 0% 1.1x',
+      time: 2
+  }, {
+      src: 'diamond4.jpg',
+      alt: 'Rubber Ring',
+      from: '100% 50%',
+      to: '30% 50% 1.5x',
+      time: 2
+  }
+], function (idx, img, idxOut, imgOut) {
+    if (idxOut == undefined) {
+        // starting single image phase, put up caption
+        $('#slidinggallery.caption').text(img.alt).animate({ opacity: .7 })
+        console.log(img.alt);
+    }
+    else {
+        // starting cross-fade phase, take out caption
+        $('#slidinggallery.caption').fadeOut()
+    }
+});
 
 
-//    $('#slideshow').crossSlide({
-//        sleep: 2,
-//        fade: 1
-//    }, [
-//      { src: 'images/diamonds/diamond1.jpg' },
-//      { src: 'images/diamonds/diamond2.jpg' },
-//      { src: 'images/diamonds/diamond3.jpg' },
-//      { src: 'images/diamonds/diamond4.jpg' }
-//    ]);
-
-//});
+});

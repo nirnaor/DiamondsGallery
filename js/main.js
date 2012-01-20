25a80fc5ad71d61031f8ec6a26ee154f5cc23bcc
Nir Naor <nirnaori@gmail.com>
2012-01-20 17:25:49 +0200

    changed the design completley

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-    showHomeAndHideOtherDivs();
 
     function showHomeAndHideOtherDivs() {
         $('#gallery').hide();
@@ -11,63 +10,46 @@ $(document).ready(function () {
 
 
 
-    $('#slidinggallery').crossSlide({
-        fade: 1
-    }, [
-  {
-      src: 'diamond1.jpg',
-      alt: 'Sand Castle',
-      from: '100% 80% 1x',
-      to: '100% 0% 1.7x',
-      time: 3
-  }, {
-      src: 'diamond2.jpg',
-      alt: 'Sunflower',
-      from: 'top left',
-      to: 'bottom right 1.5x',
-      time: 2
-  }, {
-      src: 'diamond3.jpg',
-      alt: 'Flip Flops',
-      from: '100% 80% 1.5x',
-      to: '80% 0% 1.1x',
-      time: 2
-  }, {
-      src: 'diamond4.jpg',
-      alt: 'Rubber Ring',
-      from: '100% 50%',
-      to: '30% 50% 1.5x',
-      time: 2
-  }
-], function (idx, img, idxOut, imgOut) {
-    if (idxOut == undefined) {
-        // starting single image phase, put up caption
-        $('#slidinggallery.caption').text(img.alt).animate({ opacity: .7 })
-    }
-    else {
-        // starting cross-fade phase, take out caption
-        $('#slidinggallery.caption').fadeOut()
-    }
-});
+//    $('#slidinggallery').crossSlide({
+//        fade: 1
+//    }, [
+//  {
+//      src: 'diamond1.jpg',
+//      alt: 'Sand Castle',
+//      from: '100% 80% 1x',
+//      to: '100% 0% 1.7x',
+//      time: 3
+//  }, {
+//      src: 'diamond2.jpg',
+//      alt: 'Sunflower',
+//      from: 'top left',
+//      to: 'bottom right 1.5x',
+//      time: 2
+//  }, {
+//      src: 'diamond3.jpg',
+//      alt: 'Flip Flops',
+//      from: '100% 80% 1.5x',
+//      to: '80% 0% 1.1x',
+//      time: 2
+//  }, {
+//      src: 'diamond4.jpg',
+//      alt: 'Rubber Ring',
+//      from: '100% 50%',
+//      to: '30% 50% 1.5x',
+//      time: 2
+//  }
+//], function (idx, img, idxOut, imgOut) {
+//    if (idxOut == undefined) {
+//        // starting single image phase, put up caption
+//        $('#slidinggallery.caption').text(img.alt).animate({ opacity: .7 })
+//    }
+//    else {
+//        // starting cross-fade phase, take out caption
+//        $('#slidinggallery.caption').fadeOut()
+//    }
+//});
 
 
-    $('a').click(function (e) {
-        var divToShow = $(this).data('address');
-        console.log('will now display the ' + divToShow);
-        $('div.activediv').hide();
-        $('.activediv').removeClass('activediv');
-        $('#' + divToShow).addClass('activediv');
-        $(this).addClass('activediv');
-        $('.activediv').show();
-
-        if (divToShow == 'gallery') {
-            initGalleryFlow();
-        }
-    });
-
-
-
-    var myNewFlow;
 
 
     function initGalleryFlow() {

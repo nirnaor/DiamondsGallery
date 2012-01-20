1b778441f6ea90a61c6cc3ea211be8a4b3465db5
Nir Naor <nirnaori@gmail.com>
2012-01-20 18:19:44 +0200

    added the sliding gallery

--- js/main.js
+++ js/main.js
@@ -10,44 +10,44 @@ $(document).ready(function () {
 
 
 
-//    $('#slidinggallery').crossSlide({
-//        fade: 1
-//    }, [
-//  {
-//      src: 'diamond1.jpg',
-//      alt: 'Sand Castle',
-//      from: '100% 80% 1x',
-//      to: '100% 0% 1.7x',
-//      time: 3
-//  }, {
-//      src: 'diamond2.jpg',
-//      alt: 'Sunflower',
-//      from: 'top left',
-//      to: 'bottom right 1.5x',
-//      time: 2
-//  }, {
-//      src: 'diamond3.jpg',
-//      alt: 'Flip Flops',
-//      from: '100% 80% 1.5x',
-//      to: '80% 0% 1.1x',
-//      time: 2
-//  }, {
-//      src: 'diamond4.jpg',
-//      alt: 'Rubber Ring',
-//      from: '100% 50%',
-//      to: '30% 50% 1.5x',
-//      time: 2
-//  }
-//], function (idx, img, idxOut, imgOut) {
-//    if (idxOut == undefined) {
-//        // starting single image phase, put up caption
-//        $('#slidinggallery.caption').text(img.alt).animate({ opacity: .7 })
-//    }
-//    else {
-//        // starting cross-fade phase, take out caption
-//        $('#slidinggallery.caption').fadeOut()
-//    }
-//});
+    $('#slidinggallery').crossSlide({
+        fade: 1
+    }, [
+  {
+      src: 'diamond1.jpg',
+      alt: 'Sand Castle',
+      from: '100% 80% 1x',
+      to: '100% 0% 1.7x',
+      time: 3
+  }, {
+      src: 'diamond2.jpg',
+      alt: 'Sunflower',
+      from: 'top left',
+      to: 'bottom right 1.5x',
+      time: 2
+  }, {
+      src: 'diamond3.jpg',
+      alt: 'Flip Flops',
+      from: '100% 80% 1.5x',
+      to: '80% 0% 1.1x',
+      time: 2
+  }, {
+      src: 'diamond4.jpg',
+      alt: 'Rubber Ring',
+      from: '100% 50%',
+      to: '30% 50% 1.5x',
+      time: 2
+  }
+], function (idx, img, idxOut, imgOut) {
+    if (idxOut == undefined) {
+        // starting single image phase, put up caption
+        $('#slidinggallery.caption').text(img.alt).animate({ opacity: .7 })
+    }
+    else {
+        // starting cross-fade phase, take out caption
+        $('#slidinggallery.caption').fadeOut()
+    }
+});
 
 
 

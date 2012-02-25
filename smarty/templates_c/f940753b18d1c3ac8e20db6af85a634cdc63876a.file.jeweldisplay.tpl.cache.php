a362f9a3dabfa55c42a0c9c615bda01795a1cf04
Nir Naor <nirnaori@gmail.com>
2012-02-25 18:53:11 +0200

    jeweldisplay is now using templates and php!

---
+++ smarty/templates_c/f940753b18d1c3ac8e20db6af85a634cdc63876a.file.jeweldisplay.tpl.cache.php
@@ -0,0 +1,105 @@
+<?php /* Smarty version Smarty-3.1.8, created on 2012-02-25 16:52:13
+         compiled from "../templates\jeweldisplay.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:247084f49115b4ebe49-05514856%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'f940753b18d1c3ac8e20db6af85a634cdc63876a' => 
+    array (
+      0 => '../templates\\jeweldisplay.tpl',
+      1 => 1330188731,
+      2 => 'file',
+    ),
+    'ee4186df630c936001c077ec9e0190a9a58c0484' => 
+    array (
+      0 => '../templates\\master.tpl',
+      1 => 1330185379,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '247084f49115b4ebe49-05514856',
+  'function' => 
+  array (
+  ),
+  'cache_lifetime' => 3600,
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_4f49115b56f579_33924612',
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4f49115b56f579_33924612')) {function content_4f49115b56f579_33924612($_smarty_tpl) {?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+    <title>Kre8tive Collection</title>
+    <link rel="Shortcut Icon" href="../images/diamondskullnoback.ico" />
+    <link type="text/css" rel="Stylesheet" href="../css/default.css" />
+    <script language="JavaScript" type="text/javascript" src="../js/jquery-1.7.1.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Eater|Fredericka+the+Great|Just+Me+Again+Down+Here|Asul|Amaranth' rel='stylesheet' type='text/css'/>
+    <link href='http://fonts.googleapis.com/css?family=Bilbo+Swash+Caps|Merienda+One' rel='stylesheet' type='text/css'>
+   
+     <link type="text/css" rel="Stylesheet" href="../css/jeweldisplay.css" />
+
+</head>
+<body>
+    <div id="main">
+        <div id="logoandmenu">
+        <div id="banner">
+              <img src="../images/blacklogo.gif" alt="skull" />
+            </div>
+            <div id="menu">
+                <ul id="menulist">
+                    <li><a href="gallery.html">Rings</a> </li>
+                    <li><a href="gallery.html">Necklaces</a> </li>
+                    <li><a href="contactus.html">Bracelets</a> </li>
+                    <li><a href="contactus.html">Earings</a> </li>
+                    <li><a href="about.html">Contact Us</a> </li>
+                </ul>
+            </div>
+            
+            
+        </div>
+        <div id="content">
+        
+        
+
+           <img id="mainimage" src="../images/gallery/butterfly/butterfly5.jpg" />
+
+          <div id="description">
+            <p>Ut euismod tempus elementum. Donec hendrerit, nisi vel elementum viverra, nisi metus pulvinar eni
+            m, eleifend fermentum lorem libero non lorem. Vestibulum ante 
+            ipsum primis in faucibus</p>
+          </div>
+
+          <div id="details">
+            <dl>
+              <dt>lectus:</dt>
+                <dd>consectetur</dd>
+              <dt>iaculis:</dt>
+                <dd>ectus</dd>
+            </dl>
+            <dt>lectus:</dt>
+              <dd>consectetur </dd>
+            <dt>iaculis:</dt>
+              <dd>ectus </dd>
+        </div>
+
+        <div id="emptydiv"></div>
+
+          <div id="birth">
+           <img src="../images/gallery/butterfly/butterfly3.jpg" />
+           <img src="../images/gallery/butterfly/butterfly8.jpg" />
+           <img src="../images/gallery/butterfly/butterfly2.jpg" />
+           <img src="../images/gallery/butterfly/butterfly12.jpg" />
+           <img src="../images/gallery/butterfly/butterfly7.jpg" />
+
+
+            
+        </div>
+        <div id="footer">
+            <h1>
+                "Seize the day, I heard him say"
+            </h1>
+        </div>
+    </div>
+</body>
+</html>
+<?php }} ?>
---
+++ templates/jeweldisplay.tpl
@@ -0,0 +1,37 @@
+{extends file="master.tpl"}
+{block name=scriptsandlinks}
+     <link type="text/css" rel="Stylesheet" href="../css/jeweldisplay.css" />
+{/block}
+{block name=htmlcontent}
+
+           <img id="mainimage" src="../images/gallery/butterfly/butterfly5.jpg" />
+
+          <div id="description">
+            <p>Ut euismod tempus elementum. Donec hendrerit, nisi vel elementum viverra, nisi metus pulvinar eni
+            m, eleifend fermentum lorem libero non lorem. Vestibulum ante 
+            ipsum primis in faucibus</p>
+          </div>
+
+          <div id="details">
+            <dl>
+              <dt>lectus:</dt>
+                <dd>consectetur</dd>
+              <dt>iaculis:</dt>
+                <dd>ectus</dd>
+            </dl>
+            <dt>lectus:</dt>
+              <dd>consectetur </dd>
+            <dt>iaculis:</dt>
+              <dd>ectus </dd>
+        </div>
+
+        <div id="emptydiv"></div>
+
+          <div id="birth">
+           <img src="../images/gallery/butterfly/butterfly3.jpg" />
+           <img src="../images/gallery/butterfly/butterfly8.jpg" />
+           <img src="../images/gallery/butterfly/butterfly2.jpg" />
+           <img src="../images/gallery/butterfly/butterfly12.jpg" />
+           <img src="../images/gallery/butterfly/butterfly7.jpg" />
+
+{/block}

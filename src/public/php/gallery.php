bb108e1672caea2e230b18ab8296c452a48d0baa
Nir Naor <nirnaori@gmail.com>
2012-03-10 02:34:35 +0200

    refactored gallery

--- src/public/php/gallery.php
+++ src/public/php/gallery.php
@@ -1,22 +1,20 @@
 <?php
   require('getAllFilesInDirectory.php');
+  require('setup.php');
+  require('const.php');
+
   $category = $_GET["category"];
+
   function getJewelsForCategory($category)
   {
     $dir = GALLERY_PATH . $category . "/";
     return getAllFiles($dir);
   }
 
-  require('setup.php');
-  require('const.php');
-  
   $images = json_encode(getJewelsForCategory($category));
 
-  // if you want to use the traditional object parameter format, pass a boolean of false
   $smarty = new Smarty_GuestBook();
   $smarty->caching = 0;
   $smarty->assign('imagesArray', $images);
   $smarty->display('gallery.tpl');
-
-
 ?>

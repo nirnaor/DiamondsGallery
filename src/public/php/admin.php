0a73b61e5aa8a3019af5fcdcd8df5c0fce324293
Nir Naor <nirnaori@gmail.com>
2012-04-10 20:19:42 +0300

    added all the jewels to the admin screen for later selection

--- src/public/php/admin.php
+++ src/public/php/admin.php
@@ -1,6 +1,33 @@
 <?php
-require('setup.php');
-require('authorize.php');
-$smarty = new Smarty_GuestBook();
-$smarty->display('admin.tpl');
+  require('getAllFilesInDirectory.php');
+  require('setup.php');
+  require('const.php');
+  require('jewel.php');
+  require('../../configs/db.php');
+  require('authorize.php');
+
+
+  function getAllJewels()
+  {
+    $result = array();
+    $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $query = "SELECT * from jewels";
+    $queryResult = $dbc->query($query);
+    
+    while ($row = $queryResult->fetch_assoc()) {
+      $jewel = new Jewel();
+      $jewel->fillDataFromDb($row);
+      array_push($result, $jewel);
+    }
+
+    return $result;
+  }
+
+
+  $images = json_encode(getAllJewels());
+
+  $smarty = new smarty_guestbook();
+  $smarty->caching = false;
+  $smarty->assign('imagesArray', $images);
+  $smarty->display('admin.tpl');
 ?>

7d341681a2bb2c9cfcc2d8caec9fe77de60ed8b4
Nir Naor <nirnaori@gmail.com>
2012-04-11 16:28:45 +0300

    all addjewel in now being filled with the correct metadata

--- src/public/js/addjewel.js
+++ src/public/js/addjewel.js
@@ -1,24 +1,14 @@
 $(document).ready(function () {
 
-$('[name="jewelname"]').attr('value','nir')
-$('[name="description"]')
   function fillMetadata(){
     $('[name="jewelname"]').attr('value',window.jewelDetails[0]['jewelName']);
     $('[name="description"]').html(window.jewelDetails[0]['desc']);
     $('[name="category"]').attr('value',window.jewelDetails[0]['category']);
-
-
-
-
-
-    $('[name="jewelname"]').text();
-    $('#clarity').text(window.jewelDetails[0]['clarity']);
-    $('#weight').text(window.jewelDetails[0]['weight'] + ' kt');
-    $('#cut').text(window.jewelDetails[0]['cut']);
-
-    var metalText = window.jewelDetails[0]['metalColor'] + 
-      ' ' + window.jewelDetails[0]['metalWeight'] + ' kt';
-    $('#metal').text(metalText);
+    $('[name="metalcolor"]').attr('value',window.jewelDetails[0]['metalColor']);
+    $('[name="metalweight"]').attr('value',window.jewelDetails[0]['metalWeight']);
+    $('[name="weight"]').attr('value',window.jewelDetails[0]['weight']);
+    $('[name="clarity"]').attr('value',window.jewelDetails[0]['clarity']);
+    $('[name="cut"]').attr('value',window.jewelDetails[0]['cut']);
   }
   
 
